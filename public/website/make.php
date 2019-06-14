659aef9ec3a452defba3f418cab432c512427e62
SpellingBee278 <babsyumar@gmail.com>
2019-06-14 17:15:15 +0100

    Added car.php

--- public/website/make.php
+++ public/website/make.php
@@ -4,10 +4,32 @@ class Animal {
 
 public function Move() {
 
-    echo " the animal is moving";
+    echo " the animal is walking";
+
 
 }
+
+}
+class Fish extends Animal {
+
+    public function Move() {
+
+        echo " the fish is swimming";
+    }
+
+}
+
+class Dog extends Animal {
+
 
 }
 $Animal = new Animal();
 $Animal->Move();
+
+$Fish = new Fish();
+$Fish->Move();
+
+$Dog =  new Dog();
+$Dog->Move();
+
+
