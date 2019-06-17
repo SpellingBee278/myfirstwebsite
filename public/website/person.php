7f0fa63e5833924ebd362e523bb9d99557a532bf
SpellingBee278 <babsyumar@gmail.com>
2019-06-17 17:52:30 +0100

    Added new file called person.php more info in file person.php

--- public/website/person.php
+++ public/website/person.php
@@ -10,13 +10,46 @@ class Person
          return 'Goodbye';
 
      }
+     public function walk(){
+        return 'Walking past bye';
+
+
+     }
+     public function newPerson(){
+
+
+     }
+     public function adopt(){
+        return 'Adopted a fish';
+
+     }
+     public function buy(){
+        return 'Bought fish food';
+
+     }
+    public function run(){
+        return 'The person ran';
+
+    }
 
 
 }
 
 $person = new Person();
+echo 'A new person has been made'  . "<br>";;
+echo '<h2>Person info</h2>';
 echo $person->sayGoodbye() . "<br>";;
-echo $person->sayHello() . "<br>";;
 echo 'The person has said Hello' . "<br>";;
 echo $person->sayHello() . "<br>";;
-echo 'Now the person has said Goodbye';
+echo 'Now the person has said Goodbye'  . "<br>";;
+echo $person->walk()  . "<br>";;
+echo 'The person is walking past a shop man near a sign'  . "<br>";;
+echo $person->adopt()  . "<br>";;
+echo 'The person has adopted a fish'  . "<br>";;
+echo $person->buy()  . "<br>";;
+echo 'The person has bought fish food from a pet shop'  . "<br>";;
+echo $person->run() . "<br>";;
+echo 'The person is running again' . "<br>";;
+
+
+
