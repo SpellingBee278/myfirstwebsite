f48e30a421fb19b8d35f4adf91106393d6256bf8
SpellingBee278 <babsyumar@gmail.com>
2019-07-22 17:05:49 +0100

    did more then 4 lines

--- public/website/random.php
+++ public/website/random.php
@@ -2,4 +2,12 @@
 echo'hello';
 ob_clean();
 echo 'This is a car, It has 5 seats and 4 doors';
+?>
 
+<script>
+
+    document.write('The car is a Volkswagen');
+    alert('The web has a little effort')
+    confirm('Are you sure you want to go to this website?')
+
+</script>
