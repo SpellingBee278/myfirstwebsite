8557908e36000229bf9719177affbece09e02c85
SpellingBee278 <babsyumar@gmail.com>
2019-07-23 13:40:10 +0100

    Added new file called random.html

--- public/website/random.php
+++ public/website/random.php
@@ -7,7 +7,33 @@ echo 'This is a car, It has 5 seats and 4 doors';
 <script>
 
     document.write('The car is a Volkswagen');
-    alert('The web has a little effort')
-    confirm('Are you sure you want to go to this website?')
+    document.write('<br>');
+    alert('The web has a little effort');
+    confirm('Are you sure you want to go to this website?');
+
+</script>
+<?php
+
+echo 'This is a car, It has 5 seats and 4 doors';
+?>
+<script>
+
+    document.write(' The car is a Toyota');
+    document.write('<br>');
+   alert('The system of this code is 24 lines');
+    confirm('The website does not look like that');
+
+</script>
+<?php
+
+echo 'This is a car It has 5 seats and 4 doors';
+?>
+<script>
+    document.write(' The car is a BMW');
+    document.write('<br>');
+    let car = "Golf";
+    let model = "Volkswagen";
+
+
 
 </script>
