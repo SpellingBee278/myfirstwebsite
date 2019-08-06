41dc815918c3f104a219f8a48fb7d10dfc2ce2e2
SpellingBee278 <babsyumar@gmail.com>
2019-08-06 10:36:10 +0100

    no alerts

--- public/website/random.php
+++ public/website/random.php
@@ -6,28 +6,9 @@
 echo'hello';
 ob_clean();
 echo 'This is a car, It has 5 seats and 4 doors';
-?>
-
-<script>
-
-    document.write('The car is a Volkswagen');
-    document.write('<br>');
-    alert('The web has a little effort');
-    confirm('Are you sure you want to go to this website?');
-
-</script>
-<?php
 
 echo 'This is a car, It has 5 seats and 4 doors';
 ?>
-<script>
-
-    document.write(' The car is a Toyota');
-    document.write('<br>');
-   alert('The system of this code is 24 lines');
-    confirm('The website does not look like that');
-
-</script>
 <?php
 
 echo 'This is a car It has 5 seats and 4 doors';
