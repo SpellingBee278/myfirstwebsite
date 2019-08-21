579dfc2d46457447e7300a94310681a91e590095
SpellingBee278 <babsyumar@gmail.com>
2019-08-21 19:15:53 +0100

    new index for public directory no need that much code never ever gonna be used

--- public/index.php
+++ public/index.php
@@ -1,3 +1,13 @@
-<?php
-echo 'The index of the public directory';
-ob_clean();
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CoolCoderKid</title>
+</head>
+<body>
+<a href="website">on your left is the link to the website - Link to my website</a>
+</body>
+</html>
