e2be7f7939d428dd3daa55dead2e27c1196f0141
SpellingBee278 <babsyumar@gmail.com>
2019-08-28 18:39:51 +0100

    This is 100daysofcode 2-3/100

--- public/website/100days/day2.php
+++ public/website/100days/day2.php
@@ -1,47 +1,51 @@
 <?php
-    class superHero{
+    class superHero
+    {
         public $day2code;
         public $name;
         public $power;
         public $size;
         public $favorite_color;
 
-        public function giveName($name){
-           $this->name = $name;
-            echo "Super Hero name set to: " .$this->name;
+        public function giveName($name)
+        {
+            $this->name = $name;
+            echo "Super Hero name set to: " . $this->name;
             echo "<br>";
 
         }
 
-        public function giveMagic($power){
+        public function giveMagic($power)
+        {
             $this->power = $power;
             echo " His power is set to: " . $this->power;
             echo "<br>";
 
         }
+
+
+        public function giveSize($size)
+        {
+            $this->size = $size;
+            echo "His size is set to:   " . $this->size;
+            echo "<br>";
+
+
+        }
+
+
+        public function giveColor($favorite_color)
+        {
+            $this->favorite_Color = $favorite_color;
+            echo "His favorite color is set to:     " . $this->favorite_Color;
+            echo "<br>";
+        }
     }
-    $superHero = new superHero();{
+
+    $superHero = new superHero();
     $superHero->giveName('Superman');
     $superHero->giveMagic('Fire');
-}
-
-
-    public function giveSize($size)
-{
-    $this->size = $size;
-    echo "His size is set to:   " . $this->size;
-    echo "<br>";
-
-
-}
-
-
-
-public function giveColor($favorite_color){
-  $this->favorite_Color = $favorite_color;
-  echo "His favorite color is set to:     "  . $this->favorite_Color;
-  echo "<br>";
-}
+    $superHero->giveColor('Cyan');
 
 
 
